7d7ce3c75993d2c4f0ce7bb5b2b52190004d2d4b
Santiago Acuña <snt-26@hotmail.com>
2022-07-09 21:59:25 -0500

    Crete the arquitecture to the solution

---
+++ HorariosProfesores/HorariosProfesoresWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HorariosProfesoresWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
